d9da705f2a64978c6542689dcf0af7ad5b072d10
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-10-18 08:48:50 -0400

    Add the Die and the RollAndSumDice files.

---
+++ src/main/java/practicalsix/Die.java
@@ -0,0 +1,31 @@
+// TODO: Add the correct package declaration
+
+// TODO: Add all of the required JavaDoc comments to this class
+
+public class Die {
+
+  private static final int MAX = 6;
+  private int faceValue;
+
+  public Die() {
+    faceValue = 1;
+  }
+
+  public int roll() {
+    // TODO: Add the implementation of this method
+  }
+
+  public void setFaceValue(int value) {
+    // TODO: Add the implementation of this method
+  }
+
+  public int getFaceValue() {
+    // TODO: Add the implementation of this method
+  }
+
+  public String toString() {
+    String result = Integer.toString(faceValue);
+    return result;
+  }
+
+}
---
+++ src/main/java/practicalsix/RollAndSumDice.java
@@ -0,0 +1,20 @@
+// TODO: Add the correct package declaration
+
+// TODO: Add all of the required JavaDoc comments to this class
+
+public class RollAndSumDice {
+
+  public static void main(String[] args) {
+    // TODO: Step One: Declare three instances of the Die class
+
+    // TODO: Step Two: Construct each of the three Die objects
+
+    // TODO: Step Three: Roll each of the die
+
+    // TODO: Step Four: Print out the face value of each die
+
+    // TODO: Step Five: Compute and output the sum of each die's face value
+
+  }
+
+}
